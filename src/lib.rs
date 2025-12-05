229ca779f83310ec9fdbe779e4783fa3201dcff0
Jack O'Connor <oconnor663@gmail.com>
2025-12-04 21:53:35 -0800

    first attempt at then_concurrent, quite complicated

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
 use futures::channel::mpsc::{Receiver, Sender, channel};
-use futures::{SinkExt, Stream, StreamExt, join};
-use std::pin::pin;
+use futures::future::{MaybeDone, maybe_done};
+use futures::{FutureExt, SinkExt, Stream, StreamExt, join, pending, poll};
+use std::collections::VecDeque;
+use std::pin::{Pin, pin};
 
 fn rendezvous_channel<T>() -> (RendezvousSender<T>, Receiver<T>) {
     let (sender, receiver) = channel(0);
     (RendezvousSender { sender }, receiver)
 }
 
+#[derive(Clone)]
 struct RendezvousSender<T> {
     sender: Sender<T>,
 }
@@ -23,6 +26,73 @@ impl<T> RendezvousSender<T> {
             .send(item)
             .await
             .expect("receiver should not drop");
+    }
+
+    async fn wait_for_space(&mut self) {
+        std::future::poll_fn(|cx| self.sender.poll_ready(cx))
+            .await
+            .expect("receiver should not drop");
+    }
+}
+
+struct ConcurrentFutures<Fut: Future> {
+    futures: VecDeque<Pin<Box<MaybeDone<Fut>>>>,
+}
+
+impl<Fut: Future> ConcurrentFutures<Fut> {
+    fn new() -> Self {
+        Self {
+            futures: VecDeque::new(),
+        }
+    }
+
+    fn len(&self) -> usize {
+        self.futures.len()
+    }
+
+    fn push_back(&mut self, fut: Fut) {
+        self.futures.push_back(Box::pin(maybe_done(fut)));
+    }
+
+    // Drive all the contained futures without popping any outputs. This function never completes,
+    // and it's intended to be used in a `select!` or similar with something else that will
+    // complete. If any of the contained futures are pending, then they will register for a wakeup.
+    // If not (either because all the contained futures are ready, or because this container is
+    // empty), then this function's future will yield without ever waking again.
+    //
+    // This method is cancel-safe.
+    async fn poll_without_popping(&mut self) -> ! {
+        loop {
+            for fut in &mut self.futures {
+                let _ = poll!(fut.as_mut());
+            }
+            pending!();
+        }
+    }
+
+    // Drive all the contained futures until the first one (in order) is ready, and then remove it
+    // and return its output. If this container is empty, return `None`.
+    //
+    // This method is cancel-safe.
+    async fn pop_front(&mut self) -> Option<Fut::Output> {
+        if self.futures.is_empty() {
+            return None;
+        }
+        loop {
+            // Poll all the futures once. This is a no-op for MaybeDone futures that are already
+            // done.
+            for fut in &mut self.futures {
+                let _ = poll!(fut.as_mut());
+            }
+            // If the first future is done, return its output.
+            if let Some(output) = self.futures[0].as_mut().take_output() {
+                self.futures.pop_front();
+                return Some(output);
+            }
+            // Otherwise at least the first future (and possibly others) has registered a wakeup.
+            // Yield and continue the loop after we wake up.
+            pending!();
+        }
     }
 }
 
@@ -58,6 +128,71 @@ impl<Fut: Future, T> AsyncPipeline<Fut, T> {
                         while let Some(input) = self.outputs.next().await {
                             let output = f(input).await;
                             sender.send_and_wait(output).await;
+                        }
+                    }
+                };
+            },
+            outputs: receiver,
+        }
+    }
+
+    pub fn then_concurrent<F, U>(self, f: F, limit: usize) -> AsyncPipeline<impl Future, U>
+    where
+        F: AsyncFn(T) -> U,
+    {
+        assert!(limit > 0, "`limit` must be at least 1");
+        let (mut sender, receiver) = rendezvous_channel();
+        AsyncPipeline {
+            future: async move {
+                join! {
+                    self.future,
+                    async move {
+                        let mut futures = ConcurrentFutures::new();
+                        let mut inputs = self.outputs.fuse();
+                        loop {
+                            let futures_len = futures.len();
+                            let get_input = async {
+                                if futures_len < limit {
+                                    if let Some(input) = inputs.next().await {
+                                        input
+                                    } else {
+                                        // If the inputs stream is finished, this future never returns.
+                                        std::future::pending().await
+                                    }
+                                } else {
+                                    // If we're already at the limit, this future never returns.
+                                    std::future::pending().await
+                                }
+                            };
+                            let send_output = async {
+                                if futures_len == 0 {
+                                    // If we haven't collected any futures yet, this future never
+                                    // returns.
+                                    std::future::pending().await
+                                }
+                                // Fusing inside a select! is normally a mistake, but in this case
+                                // I claim it's fine.
+                                futures::select!(
+                                    _ = sender.wait_for_space().fuse() => {}
+                                    // `poll_without_popping` makes sure we keep making forward
+                                    // progress, but it never returns. Only `wait_for_space` can
+                                    // complete this `join!`.
+                                    _ = futures.poll_without_popping().fuse() => {}
+                                );
+                                // At this point we're guaranteed to be able to `send`, so we can
+                                // get an output without worrying about dropping it.
+                                let output = futures.pop_front().await.expect("there should be futures, we just checked");
+                                sender.sender.start_send(output).expect("there should be space, we just checked");
+                            };
+                            futures::select! {
+                                input = pin!(get_input.fuse()) => {
+                                    futures.push_back(f(input));
+                                },
+                                _ = pin!(send_output.fuse()) => {}
+                            }
+                            if inputs.is_done() && futures.len() == 0 {
+                                return;
+                            }
                         }
                     }
                 };
@@ -203,5 +338,26 @@ mod tests {
             sleep(Duration::from_millis(1)).await;
         })
         .await;
+    }
+
+    #[tokio::test]
+    async fn test_concurrent() {
+        use std::sync::atomic::{AtomicU32, Ordering::Relaxed};
+        static FUTURES_IN_FLIGHT: AtomicU32 = AtomicU32::new(0);
+        static MAX_IN_FLIGHT: AtomicU32 = AtomicU32::new(0);
+        let v: Vec<i32> = pipeline(futures::stream::iter(0..10))
+            .then_concurrent(
+                async |i| {
+                    let in_flight = FUTURES_IN_FLIGHT.fetch_add(1, Relaxed);
+                    MAX_IN_FLIGHT.fetch_max(in_flight + 1, Relaxed);
+                    sleep(Duration::from_millis(1)).await;
+                    FUTURES_IN_FLIGHT.fetch_sub(1, Relaxed);
+                    2 * i
+                },
+                3,
+            )
+            .collect()
+            .await;
+        assert_eq!(v, vec![0, 2, 4, 6, 8, 10, 12, 14, 16, 18]);
     }
 }
